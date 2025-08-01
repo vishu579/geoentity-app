76c23a2f9e3b189e7e09fedbadc6700dd3164255
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-08-01 23:56:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/config')
+def config():
+    return render_template('config.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port='5003')
