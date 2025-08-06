ff70a949fb8a746b12e4f6a83d8bbfed6f05cca4
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-08-06 23:41:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 import json
